5a41ab6ea1df903c1f2750398c47996c0ec02481
up1 <somkiat.p@gmail.com>
2019-04-19 14:05:44 +0700

    Updarew v2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pui-helloworld-library",
-    version='1.0',
+    version='2.0',
     package_dir={'' : 'src'},
     packages=['HelloWorld'],
     url='https://github.com/up1/demo-helloworld-library',
